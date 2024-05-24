cb02d1c7af42a55d415ae2eb24fa669d58ab9c40
João Karllus Monteiro <105602561+joaokarllusMonteiro@users.noreply.github.com>
2024-05-24 16:57:23 -0300

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,9 @@
+# Array com produtos de uma loja (loja de comida)
+produtos_loja = ["maçã", "pão", "queijo", "arroz", "frango"]
+
+# Array com anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1992, 1980, 2000]
+
+# Imprimindo os arrays
+print("Produtos da loja:", produtos_loja)
+print("Anos de nascimento:", anos_nascimento)
